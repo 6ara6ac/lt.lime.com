4177fc5d5ae9055e5eb1039909b64efa4b2b3c40
6ara6ac <kirill.v.abc@gmail.com>
2022-12-28 13:56:38 +0200

    fix bugs

--- src/js/swiper-bundle.js
+++ src/js/swiper-bundle.js
@@ -24,13 +24,13 @@
       },
       // // when window width is >= 480px
       768: {
-        slidesPerView: 4,
+        slidesPerView: 2,
       },
       // // when window width is >= 640px
-      // 640: {
-      //   slidesPerView: 4,
-      //   spaceBetween: 40
-      // }
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
     },
   
 
